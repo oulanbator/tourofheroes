3bb0272264ac3d7d284938b79179ad8111d99465
Victor Matheron <victor.matheron@cgi.com>
2021-10-21 17:49:08 +0200

    Tour of Heroes tutorial - 4. Add Services - completed

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import {HEROES} from "../mock-heroes";
+import {Hero} from "../hero";
+import {HeroService} from "../hero.service";
+import {MessageService} from "../message.service";
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  heroes: Hero[] = []
+  selectedHero?: Hero;
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+  // The previous version assigns an array of heroes to the component's heroes property.
+  // The assignment occurs synchronously, as if the server could return heroes instantly
+  // or the browser could freeze the UI while it waited for the server's response.
+  // The new version waits for the Observable to emit the array of heroes—which could
+  // happen now or several minutes from now. The subscribe() method passes the emitted array
+  // to the callback, which sets the component's heroes property.
+  // This asynchronous approach will work when the HeroService requests heroes from the server.
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero =${hero.name}`);
+  }
+
+}
